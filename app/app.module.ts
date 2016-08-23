24138b2001dcb00e3f539c35bb974ad8b487ede6
Peter Hasitschka <phasitschka@know-center.at>
2016-08-23 17:35:46 +0200

    Added simple info window in sidebar

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './components/app/app.component';
+import { SideInfoComponent }  from './components/app/sideinfo/sideinfo.component';
 import { GraphworkspaceComponent }  from './components/graphworkspace/graphworkspace.component';
 import { PlaneComponent }  from './components/plane/plane.component';
 
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent, GraphworkspaceComponent, PlaneComponent],
+    declarations: [AppComponent, GraphworkspaceComponent, PlaneComponent, SideInfoComponent],
     bootstrap: [AppComponent]
 })
 /**
---
+++ app/components/app/sideinfo/sideinfo.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+
+
+
+@Component({
+    selector: 'sideinfo',
+    templateUrl: 'app/components/app/sideinfo/sideinfo.html',
+    styleUrls: ['app/components/app/sideinfo/sideinfo.css']
+})
+/**
+ * Little window in sidebars for showing information
+ */
+export class SideInfoComponent {
+
+    public title: string;
+    public text: string;
+    @Input() identifier: string;
+    constructor() {
+        this.title = "title";
+        this.text = "text";
+    }
+}
